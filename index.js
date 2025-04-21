8ab9282a3ab11b4f76d8317d310716458dae30c9
Abdallaha Rayan <abdallaha.rayan.1999@gmail.com>
2025-04-21 13:36:23 +0300

    api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const authRoutes = require('./routes/authRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 
@@ -14,9 +17,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // المسارات
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/auth', authRoutes);
 app.use('/api/reports', reportRoutes);
-
+app.get('/', (req, res) => {
+    res.send('API is running. Visit /api-docs for Swagger documentation.');
+  });
+  
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
